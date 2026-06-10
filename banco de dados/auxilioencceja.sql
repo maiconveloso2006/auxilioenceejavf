17195c3dcfe9a894375fbe508332b2cc74b84c97
maiconveloso2006 <maicondelimavelosoveloso@gmail.com>
2026-06-10 19:41:25 -0300

    Add files via upload

--- banco de dados/auxilioencceja.sql
+++ banco de dados/auxilioencceja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02/06/2026 às 15:35
+-- Tempo de geração: 10/06/2026 às 16:25
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
